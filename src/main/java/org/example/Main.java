757e38a4bf714bafdf4419d1c679e8515061c399
Yernur <ernursyrlibayev@gmail.com>
2025-11-15 08:31:54 +0500

    forget to create inputs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.example.algorithm.KMPMatcher;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        run("small");
+        run("medium");
+        run("large");
+    }
+
+    private static void run(String size) throws Exception {
+        String inputPath = "data/input/input_" + size + ".json";
+        String outputPath = "data/output/output_" + size + ".json";
+
+        JsonObject input = new Gson().fromJson(new FileReader(inputPath), JsonObject.class);
+        String text = input.get("text").getAsString();
+        String pattern = input.get("pattern").getAsString();
+
+        long start = System.nanoTime();
+        KMPMatcher kmp = new KMPMatcher(pattern);
+        List<Integer> positions = kmp.search(text);
+        long end = System.nanoTime();
+
+        JsonObject result = new JsonObject();
+        result.addProperty("size", size);
+        result.addProperty("pattern", pattern);
+        result.addProperty("matches", positions.size());
+        result.addProperty("execution_ns", end - start);
+        result.add("positions", new Gson().toJsonTree(positions));
+
+        Files.createDirectories(Paths.get("data/output"));
+        FileWriter fw = new FileWriter(outputPath);
+        fw.write(new Gson().toJson(result));
+        fw.close();
+
+        System.out.println("Finished: " + size + " | matches=" + positions.size() + " | time=" + (end - start));
     }
 }
